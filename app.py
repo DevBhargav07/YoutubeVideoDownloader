f3c6a01dc7bd351aa9662e670c68e07125ad17d8
DevBhargav07 <bhargavdevarampati24@gmail.com>
2026-01-23 22:23:36 +0530

    adding the code to download the file

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, render_template, send_file
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DOWNLOAD_DIR = os.path.join(BASE_DIR, "VIDEO_DIR")
+os.makedirs(DOWNLOAD_DIR, exist_ok=True)
 
 @app.route('/', methods=['GET', 'POST'])
 def index() -> any:
@@ -27,14 +31,28 @@ def add(num1: any, num2: any) -> int:
         return f"Both inputs must be numbers but found {num1, num2}"
     return f'{num1} + {num2} = {num1 + num2}'
 
-@app.route('/download/')
+@app.route('/download-video/', methods=["POST"])
 def download_vide0() -> any:
     url = request.args.get('url')
+    print(f'url is :{url}')
+    if not url: url = request.form.get('yturl')
+    print(url)
     try:
         yt = YouTube(url, on_progress_callback=on_progress)
         video = yt.streams.get_highest_resolution()
-        return video.download()
+        file_path = os.path.join(DOWNLOAD_DIR, f"{video.download()}")
+        print(f'file path is {os.path.abspath(file_path)}')
+        return send_file(
+            file_path,
+            as_attachment=True,
+            download_name=f"{yt.title}",
+            mimetype="video/mp4"
+        )
     except Exception as e:
+        # yt = YouTube(url, use_oauth=True, allow_oauth_cache=True, on_progress_callback=on_progress)
+        # video = yt.streams.get_highest_resolution()
+        # return video.download()
+        
         print(f'error: {e}')
         return f'Stopped the process in middle'
 
@@ -46,10 +64,15 @@ def download_audioonly(url: any) -> any:
         ys = yt.streams.get_audio_only()
         return  ys.download()
     except Exception as e:
+        # yt = YouTube(url, use_oauth=True, allow_oauth_cache=True, on_progress_callback=on_progress)
+        # ys = yt.streams.get_audio_only()
+        # return  ys.download()
         print(f'error: {e}')
         return f'Stopped the process in middle'
 
-
+@app.route('/download/')
+def download_page() -> any:
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5555, debug=True)
