4c92540ba96d533b4f94fcfc89b4893ae79a9b0a
DevBhargav07 <bhargavdevarampati24@gmail.com>
2026-01-25 18:37:36 +0530

    adding logic to download audio of a yt_video

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ import os
 
 app = Flask(__name__, template_folder='templates')
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DOWNLOAD_DIR = os.path.join(BASE_DIR, "VIDEO_DIR")
-os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+VIDEO_DIR = os.path.join(BASE_DIR, "VIDEO_DIR")
+AUDIO_DIR = os.path.join(BASE_DIR, 'AUDIO_DIR')
+os.makedirs(VIDEO_DIR, exist_ok=True)
+os.makedirs(AUDIO_DIR, exist_ok=True)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index() -> any:
@@ -32,17 +35,15 @@ def add(num1: any, num2: any) -> int:
     return f'{num1} + {num2} = {num1 + num2}'
 
 @app.route('/download-video/', methods=["POST"])
-def download_vide0() -> any:
+def download_video() -> any:
     url = request.args.get('url')
-    print(f'url is :{url}')
     if not url: url = request.form.get('yturl')
-    print(url)
     try:
         yt = YouTube(url, on_progress_callback=on_progress)
         video = yt.streams.get_highest_resolution()
         # file_path = os.path.join(DOWNLOAD_DIR, f"{video.download()}")
         # print(f'file path is {os.path.abspath(file_path)}')
-        file_path = video.download(output_path=DOWNLOAD_DIR)
+        file_path = video.download(output_path=VIDEO_DIR)
         return send_file(
             file_path,
             as_attachment=True,
@@ -57,13 +58,21 @@ def download_vide0() -> any:
         print(f'error: {e}')
         return f'Stopped the process in middle'
 
-@app.route('/download-audio/')
-def download_audioonly(url: any) -> any:
+@app.route('/download-audio/', methods=['POST'])
+def download_audio() -> any:
     url = request.args.get('url')
+    if not url: url = request.form.get('ytaurl')
     try:
         yt = YouTube(url, on_progress_callback=on_progress)
-        ys = yt.streams.get_audio_only()
-        return  ys.download()
+        audio = yt.streams.get_audio_only()
+        file_path = audio.download(output_path=AUDIO_DIR)
+        return send_file(
+            file_path,
+            as_attachment=True,
+            download_name=f'{yt.title}',
+            mimetype="audio/mpeg"
+        )
+        # return  ys.download()
     except Exception as e:
         # yt = YouTube(url, use_oauth=True, allow_oauth_cache=True, on_progress_callback=on_progress)
         # ys = yt.streams.get_audio_only()
